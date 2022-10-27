789e6824880d1ecf2d967daa73ede11190d02f05
imanzano79 <imanzano@cimat.mx>
2022-10-27 13:43:07 -0500

    update 2 semana2

--- JavaScript/NodeJS/Semana2/routes/auth.js
+++ JavaScript/NodeJS/Semana2/routes/auth.js
@@ -1,4 +1,4 @@
-const router = require('express').Router(); 
+const router = require('express').Router(); //utiliza el propio ruteador de express
 
 router.get('/registrar', (req, res) =>{
     res.render("registrate.ejs")

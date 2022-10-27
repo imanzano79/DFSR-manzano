bc99038ab09bca6745517360899f79daaeb124b9
imanzano79 <imanzano@cimat.mx>
2022-10-27 13:42:39 -0500

    Update seman2

--- JavaScript/NodeJS/Semana2/index.js
+++ JavaScript/NodeJS/Semana2/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose'); 
 const bodyparser = require('body-parser'); 
 
-
 const app = express(); 
-
 
 
 app.set('views engine', 'ejs'); 

e018816ce61b254f74299d8d3ed088611f0c8aea
imanzano79 <imanzano@cimat.mx>
2022-10-27 12:04:37 -0500

    semana2

--- JavaScript/NodeJS/Semana2/index.js
+++ JavaScript/NodeJS/Semana2/index.js
@@ -1,33 +1,42 @@
+const express = require('express'); 
+const mongoose = require('mongoose'); 
+const bodyparser = require('body-parser'); 
 
-const express = require('express');
-const mongoose = require ('mongoose');
-const bodyparser = require ('body-parser');
+
+const app = express(); 
 
 
-const app = express();
 
-app.use(bodyparser.urlencoded({extended:false}));
-app.use(bodyparser.json());
+app.set('views engine', 'ejs'); 
 
+app.use(bodyparser.urlencoded({extended:false})); 
+app.use(bodyparser.json()); 
+
+const authRoutes = require('./routes/auth'); 
+app.use('/api/user', authRoutes); 
 
 
 app.get('/', (req, res) =>{
-        res.json(
-            {
-                estado: true,
-                mensaje : "Conexion Exitosa" 
+    /* res.json(
+        {
+            estado: true, 
+            mensaje : "Conexion exitosa"
+        }
+    );  */
 
-            }
-               
-        )
+    res.render('index.ejs');
 });
 
 
-const PORT = process.env.PORT || 3000;
+app.use(function(req, res, next) {
+    var err = new Error('Not Found'); 
+    err.status = 404; 
+next(err); 
+
+});
+
+
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, ()=>{
-        console.log("Servidor Alzado");
-} )
-
-
-
-
+    console.log("Servidor alzado"); 
+})
---
+++ JavaScript/NodeJS/Semana2/routes/auth.js
@@ -0,0 +1,21 @@
+const router = require('express').Router(); 
+
+router.get('/registrar', (req, res) =>{
+    res.render("registrate.ejs")
+}); 
+
+router.post('/registrar', (req, res) =>{
+    let correo; 
+    let pass; 
+
+    correo = req.body.correo; 
+    pass = req.body.password;
+
+    console.log('Correo: ' , correo); 
+    console.log('Password: ', pass);
+    //res.render("registrate.ejs")
+}); 
+
+module.exports = router;
+
+
